e1024e2745da7a76d3b7acff3f45a7c5b0f65f1b
Daniel Rodriguez <daniel1946203@gmail.com>
2022-08-27 17:53:56 -0500

    Se elimino el codigo js

--- js/index.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('Alert').addEventListener('click',function () {
-    let message = document.getElementById('AlertInput').value;
-    if(message == '' || message === null) {
-        alert("¡coloca algo en el input!")
-    }else{
-        alert(message)
-    }
-})
